cf320e81544827b79b77f492c78e0bb1a4c3f55f
noomdalv <vladlunasan@gmail.com>
2020-07-10 22:41:07 -0500

    imported socket.io 1.7.3 cdn and chat.js into index.html, setup server

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require("express");
-
+var socket = require("socket.io");
 
 // App Setup
 var app = express();
@@ -9,3 +9,10 @@ var server = app.listen(4000, function() {
 
 // Static Files
 app.use(express.static("public"));
+
+// Socket Setup
+var io = socket(server);
+
+io.on("connection", function() {
+	console.log("Socket connection established")
+})
